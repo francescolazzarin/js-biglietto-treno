7c9dadca34609978c7d25d92a52ed4ce4bede640
francescolazzarin <francesco.lazzarin1996@gmail.com>
2023-12-22 16:23:02 +0100

    <feat>:creata struttura condizionale per controllo            inserimento numeri e controllo SE? minorenne

--- Js/main.js
+++ Js/main.js
@@ -1,2 +1,11 @@
 const prezzo_per_km = 0.21
+const sconto_minorenni= 20 / 100
+const sconto_over_65=40 / 100
 let età= prompt("inserisci la tua età: ")
+let km_da_percorrere=prompt("inserisci i km che vuoi percorrere ")
+if ((isNaN(età)) || (isNaN(km_da_percorrere))){
+    alert("inserisci un valore numerico!")
+}else if (età>0 && età<18){
+    let prezzo_minorenni=(Math.round(prezzo_per_km*km_da_percorrere*sconto_minorenni*100)/100)
+    document.writeln(`Sei minorenne hai diritto ad un prezzo ridotto del 20%, il prezzo del tuo biglietto è di: ${prezzo_minorenni}€`)
+}
